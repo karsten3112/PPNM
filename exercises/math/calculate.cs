0ce0b44b6182859cb837d74e723e4d081b1718fc
Karsten <karsten.walz.jensen@gmail.com>
2023-01-31 22:45:53 +0100

    31-01-2023

--- exercises/math/calculate.cs
+++ exercises/math/calculate.cs
@@ -1,6 +1,13 @@
+using static System.Console;
+using static System.Math;
+
 class calculate{
+	static double sqrt2 = Sqrt(2.0);
+	static double exp = Exp(PI);
+	static double pi = Pow(PI, Exp(1));
 	static void Main(){
-		double sqrt2 = System.Math.Sqrt(2.0);
-		System.Console.Write($"sqrt(2) = {sqrt2}\n");
+		Write($"sqrt(2) = {sqrt2}\n");
+		Write($"e^pi = {exp}\n");
+		Write($"pi^e = {pi}\n");
 	}
 }
