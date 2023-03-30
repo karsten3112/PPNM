dffc5b2a902b75a50b6437553fdc25759d658667
Karsten <karsten.walz.jensen@gmail.com>
2023-03-30 17:06:57 +0200

    fixed accidental deletes

--- exercises/vec/main.cs
+++ exercises/vec/main.cs
@@ -2,9 +2,17 @@ using static System.Console;
 using static System.Math;
 
 class main{
-	
+
 	static void Main(){
-	 Write("hello\n");
-	 Vec v = new Vec();
+	vec p = new vec();
+	vec v = new vec(1.0, 2.0, 3.0);
+	vec u = new vec(3.0, 0, 5.0);
+	WriteLine(p);
+	WriteLine(v);
+	WriteLine(u);
+	vec s = (double)(2)*v;
+	double d = v.dot(u);
+	WriteLine(d);
+	WriteLine(s);
 	}
 }
