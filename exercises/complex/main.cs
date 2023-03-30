4474b6c813f849aebfdd23623e95c6cbdfb1908d
Karsten <karsten.walz.jensen@gmail.com>
2023-03-31 00:07:38 +0200

    added complex

--- exercises/complex/main.cs
+++ exercises/complex/main.cs
@@ -14,7 +14,51 @@ class main{
 		complex iPowi = cmath.pow(var2, var2);
 		complex logi = cmath.log(var2);
 		complex sinip = cmath.sin(var2*PI);
-		WriteLine(sqrti);
+		WriteLine("Calculating sqrt(-1) ....");
+		WriteLine(sqrtn);
+		WriteLine("sqrt(-1) = +-i (should be) - comparing....");
+		WriteLine($"{sqrtn.approx(+-var2)}");
 
+		WriteLine("---------------------------------------------------");
+
+		WriteLine("Calculating sqrt(i) ....");
+		WriteLine(sqrti);
+		WriteLine("sqrt(i) = 1/sqrt(2) + i/sqrt(2) (should be) - comparing....");
+		WriteLine($"{sqrti.approx(var1/cmath.sqrt(2)+var2/cmath.sqrt(2))}");
+
+		WriteLine("---------------------------------------------------");
+
+		WriteLine("Calculating exp(i) ....");
+		WriteLine(exi);
+		WriteLine("exp(i) = cos(1) + i*sin(1) (should be) - comparing....");
+		WriteLine($"{exi.approx(cmath.cos(var1) + var2*cmath.sin(var1))}");
+
+		WriteLine("---------------------------------------------------");
+
+		WriteLine("Calculating exp(pi*i) ....");
+		WriteLine(exip);
+		WriteLine("exp(pi*i) = -1 (should be) - comparing....");
+		WriteLine($"{exip.approx(-var1)}");
+
+		WriteLine("---------------------------------------------------");
+
+		WriteLine("Calculating i^i ....");
+		WriteLine(iPowi);
+		WriteLine("i^i = e^(-pi/2) (should be) - comparing....");
+		WriteLine($"{iPowi.approx(cmath.exp(-var1*PI/2.0))}");
+
+		WriteLine("---------------------------------------------------");
+
+		WriteLine("Calculating ln(i) ....");
+		WriteLine(logi);
+		WriteLine("ln(i) = i*pi/2 (should be) - comparing....");
+		WriteLine($"{logi.approx(var2*PI/2.0)}");
+
+		WriteLine("---------------------------------------------------");
+
+		WriteLine("Calculating sin(i*pi) ....");
+		WriteLine(sinip);
+		WriteLine("sin(i*pi) = i*sinh(pi) (should be) - comparing....");
+		WriteLine($"{sinip.approx(var2*(cmath.exp(PI)-cmath.exp(-PI))/2.0)}");
 	}
 }
