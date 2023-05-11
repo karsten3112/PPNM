06b2807ea783490d7390ac284fab6516a42817ef
Karsten <karsten.walz.jensen@gmail.com>
2023-05-11 20:54:32 +0200

    added montecarlo

--- homework/montecarlo/main.cs
+++ homework/montecarlo/main.cs
@@ -3,13 +3,59 @@ using static System.Console;
 using static System.Math;
 
 class main{
-	static void Main(){
-		vector a = new vector(0.0,0.0,0.0);
-		vector b = new vector(PI,PI,PI);
-		Func<vector, double> f = delegate(vector z){
-			return 1.0/(1.0 - Cos(z[0])*Cos(z[1])*Cos(z[2]))/(PI*PI*PI);
-		};
-		(double res, double err) = montecarlo.plainMC(f, a, b, 10000000);
-		WriteLine(res);
+	static void Main(string[] args){
+		foreach(string arg in args){
+			string[] inp = arg.Split(":");
+			if(inp[0] == "-tPlainMC" || inp[0] == "-tQuasiMC"){
+				bool convergence = false;
+				double uncer = 0.0;
+				int N = 100; double res = 0.0; double err = 2.0;
+				vector a = new vector(0.0,0.0);
+				vector b = new vector(1.0,1.0);
+				Func<vector, double> f = delegate(vector z){
+					double x = z[0]; double y = z[1];
+					return x*x/(1.0+y*y);
+				};
+				while(convergence != true){
+					if(inp[0] == "-tPlainMC"){
+						(res, err) = montecarlo.plainMC(f, a, b, N);
+						uncer = 1e-3;
+					} else {
+						(res, err) = montecarlo.quasiMC(f, a, b, N);
+						uncer = 1e-6;
+					}
+					WriteLine($"{N}	{err} {res}");
+					if(err <= uncer){
+						convergence = true;
+					}
+					N+=100;
+				}
+			}
+
+			if(inp[0] == "-hard"){
+				vector a = new vector(0.0,0.0,0.0);
+				vector b = new vector(PI,PI,PI);
+				Func<vector, double> f = delegate(vector z){
+					return 1.0/(1.0 - Cos(z[0])*Cos(z[1])*Cos(z[2]))/(PI*PI*PI);
+				};
+				(double res, double err) = montecarlo.plainMC(f, a, b, 10000000);
+				WriteLine(res);
+			}
+			if(inp[0] == "-debug"){
+				for(int i = 0; i < 100;i++){
+					WriteLine($"{corput(i, 2)}");
+				}
+			}
+		}
 	}
+	public static double corput(int n, int b){
+         double q = 0.0; double bk = (double)1.0/b;
+         while(n > 0){
+             q+= (n % b)*bk;
+             n/=b;
+             bk/=b;
+         }
+         return q;
+     }
+
 }
--- homework/montecarlo/mc.cs
+++ homework/montecarlo/mc.cs
@@ -4,6 +4,8 @@ using System;
 
 
 public static class montecarlo{
+	public static int[] bs1 = {2,3,5,7,11,13};
+	public static int[] bs2 = {17,19,23,29,31};
 
 	public static (double, double) plainMC(Func<vector, double> f, vector a, vector b, int N){
 		double V = 1; double err = 0.0; Random rnd = new Random();
@@ -26,8 +28,42 @@ public static class montecarlo{
 		return (result,err);
 	}
 
-	public static void qausiMC(){
-
+	public static (double, double) quasiMC(Func<vector, double> f, vector a, vector b, int N){
+		vector vec1 = new vector(a.size); vector vec2 = new vector(a.size);
+		double sum1 = 0.0; double sum2 = 0.0; double V = 1.0; double err;
+		for(int i = 0; i < a.size; i++){
+			V*=(b[i]-a[i]);
+		}
+		for(int i = 0; i < N; i++){
+			vec1 = halton(i, bs1, a.size);
+			vec2 = halton(i, bs2, a.size);
+			for(int j = 0; j < a.size; j++){
+				vec1[j] = vec1[j]*(b[j]-a[j]) + a[j];
+				vec2[j] = vec2[j]*(b[j]-a[j]) + a[j];
+			}
+			sum1+=f(vec1); sum2+=f(vec2);
+		}
+		sum1/=N; sum2/=N;
+		sum1*=V; sum2*=V;
+		err = Abs(sum1 - sum2);
+		return (sum1,err);
 	}
 
+	public static double corput(int n, int b){
+		double q = 0.0; double bk = (double)1.0/b;
+		while(n > 0){
+			q+= (n % b)*bk;
+			n/=b;
+			bk/=b;
+		}
+		return q;
+	}
+
+	public static vector halton(int n, int[] bs, int dim){
+		vector result = new vector(dim);
+		for(int i = 0; i < dim; i++){
+			result[i] = corput(n, bs[i]);
+		}
+		return result;
+	}
 }
